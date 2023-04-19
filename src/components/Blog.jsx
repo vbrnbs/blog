cf4c6cd5ea759081ea7c98949c9a5d48845995d1
vbrnbs <varszegibarnabas@gmail.com>
2023-04-19 16:41:53 +1200

    commit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,13 +1,47 @@
-import {
-  collection,
-  onSnapshot,
-  orderBy,
-  query,
-  setDoc,
-} from "firebase/firestore";
-import React, { useEffect, useState } from "react";
-import { db } from "../firebaseConfig";
 
-const Blog = () => {};
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import Filter from "./Filter";
+
+function Blog() {
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+    }, []);
+
+console.log(postLists)
+
+return(
+    <div className="homePage">
+      {/* {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.tags.map(tag => (
+                <p>#{tag}</p>
+            ))} 
+            </div>
+            <h3>@{post.git}</h3>
+          </div>
+        );
+      })} */}
+      {/* <Filter3 data={postLists}/> */}
+    </div>
+)
+
+}
+
 
 export default Blog;
