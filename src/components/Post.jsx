d878fbbf82b66d8edf79d33972c23211d12681f3
vbrnbs <varszegibarnabas@gmail.com>
2023-05-10 15:19:07 +1200

    Post page added and reorganized

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,42 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { FilteredPostsContext } from "../hooks/useFiltering";
+import { useContext } from "react";
+
 
 const Post = () => {
   const { id } = useParams();
+  const { filteredPosts, setSelectedFilters } = useContext(FilteredPostsContext);
+
+  const post = filteredPosts.find((post) => post.id === id);
+  console.log(post);
 
   return (
     <div>
-      Post
-      {id}
+      <h1>{post.title}</h1>
+      <p className="my-2">{Date(post.createdAt.miliseconds)}</p>
+      <div className="flex justify-center items-center">
+        <img
+          className='w-img rounded-sm drop-shadow-sm object-cover'
+          src={post.imageUrl}
+          alt={post.title}
+        />
+      </div>
+      <p> {post.text} </p>
+      <div>
+        {post.tags.map((tag, idx) => (
+          <Link
+            to="/"
+            key={`#${tag}-${idx}`}
+          >
+            <button
+              href="#"
+              onClick={() => setSelectedFilters([tag])}
+            >
+             {`#${tag}`}
+            </button> 
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
